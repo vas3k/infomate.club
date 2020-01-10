2acb1993286a01c0a73862c9b287b7f558419520
Konstantin Mikhailov <jtraub@users.noreply.github.com>
2020-01-11 01:58:30 +1000

    Detect dark mode in CSS
    We duplicated css code a bit but instead users won't see white flashes
    when they navigate around the site. Also as the result might be able to
    override their system-wide dark mode settings with the theme switcher.

--- static/js/main.js
+++ static/js/main.js
@@ -2,20 +2,19 @@ function initializeThemeSwitcher() {
     const themeSwitch = document.querySelector('.theme-switcher input[type="checkbox"]');
 
     themeSwitch.addEventListener("change", function(e) {
+        let theme = 'light';
         if (e.target.checked) {
-            document.body.className = "dark-theme";
-            localStorage.setItem("theme", "dark");
-        } else {
-            document.body.className = "light-theme";
-            localStorage.setItem("theme", "light");
+            theme = 'dark';
         }
+
+        document.documentElement.setAttribute('theme', theme);
+        localStorage.setItem('theme', theme);
     }, false);
 
-    const isDarkOS = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const theme = localStorage.getItem("theme");
-    if ((theme === "dark" || isDarkOS) && theme !== "light") {
-        document.body.className = "dark-theme";
-        themeSwitch.checked = true;
+    const theme = localStorage.getItem('theme');
+    if (theme !== null) {
+        themeSwitch.checked = (theme === 'dark');
+        document.documentElement.setAttribute('theme', theme);
     }
 }
 
