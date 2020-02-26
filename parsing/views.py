430dbdbf127168fd147f4bbdb9c20e9b9674a5ef
vas3k <me@vas3k.ru>
2020-02-26 10:37:46 +0100

    Use enum instead of strings

--- parsing/views.py
+++ parsing/views.py
@@ -3,17 +3,19 @@ from django.http import Http404
 
 from parsing.exceptions import ParsingException
 from parsing.telegram.telegram import get_channel
+from parsing.telegram.models import MessageType
 
 
 class TelegramChannelFeed(Feed):
-
     FEED_ITEMS = 30
 
     def get_object(self, request, channel):
         limit = int(request.GET.get("size") or self.FEED_ITEMS)
         only = str(request.GET.get("only") or "")
         if only:
-            only = [item.strip() for item in only.split(",")]
+            only = [
+                MessageType.get(item.strip()) for item in only.split(",")
+            ]
             limit = 100  # dirty hack: artificially increase the limit to get more filtered messages
 
         try:
