3b292b6ed21b6ccce030f66af23731e0adc5498b
vas3k <me@vas3k.ru>
2021-04-29 14:19:45 +0300

    Remove debug print

--- scripts/filters.py
+++ scripts/filters.py
@@ -2,7 +2,6 @@ def echomsk_title_fix(entry):
     title = entry.get("title")
     if len(title) > 20 and title[17] == ":":
         entry["title"] = title[19:]
-        print("NEW TITLE", entry["title"])
     return entry
 
 
