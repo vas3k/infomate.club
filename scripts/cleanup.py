2ab857aae0ac65213a0473320579626472241be1
vas3k <me@vas3k.ru>
2022-08-15 12:11:50 +0200

    scripts: improve cleanup script

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -9,16 +9,34 @@ django.setup()
 import click
 from datetime import datetime, timedelta
 
-from boards.models import Article
+from django.conf import settings
 
-
-DEFAULT_CLEANUP_DAYS = 300
+from boards.models import Article, BoardFeed
 
 
 @click.command()
-@click.option('--older-than-days', default=DEFAULT_CLEANUP_DAYS, help="Num days to cleanup older articles")
-def cleanup(older_than_days):
+@click.option(
+    '--older-than-days',
+    default=settings.OLD_ARTICLES_CLEANUP_AFTER_DAYS,
+    help="Num days to cleanup older articles"
+)
+@click.option(
+    '--more-than-amount',
+    default=settings.OLD_ARTICLES_CLEANUP_AFTER_AMOUNT,
+    help="Max amount of articles allowed in feed"
+)
+def cleanup(older_than_days, more_than_amount):
+    click.echo(f"Cleaning up articles older than {older_than_days} days...")
     Article.objects.filter(created_at__lte=datetime.utcnow() - timedelta(days=older_than_days)).delete()
+    for feed in BoardFeed.objects.all():
+        click.echo(f"Cleaning up feed {feed.name}, leaving {more_than_amount} last articles...")
+        last_article_to_leave = Article.objects\
+            .filter(feed=feed)\
+            .order_by("created_at")[more_than_amount:more_than_amount + 1].first()
+        if last_article_to_leave:
+            num_deleted, _ = Article.objects.filter(feed=feed, created_at__gt=last_article_to_leave.created_at).delete()
+            click.echo(f"Deleted {num_deleted} old articles!")
+    click.echo("Done")
 
 
 if __name__ == '__main__':
